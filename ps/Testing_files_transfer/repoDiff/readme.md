1df23f1dbb3077d61bbd29252da5438db51789b4
Suresh Venkatesan <7613305+sureshvenkatesan@users.noreply.github.com>
2026-04-24 07:43:52 -0700

    Added repoDiff

---
+++ ps/Testing_files_transfer/repoDiff/readme.md
@@ -0,0 +1,227 @@
+# Artifactory Repository Data Comparison
+
+The [repodiff.py](repodiff.py) is designed to compare artifacts between source and target repositories in  different 
+JFrog Artifactory instances and identifies artifacts present in the source repository but missing in the target repository. It fetches the list of 
+artifacts from both source and target repositories , compares the URIs (finds the diff  based on repo path and the 
+`sha1` checksum) , and generates reports of missing artifacts and statistics.
+
+It also provides additional features such as extracting and sorting artifact statistics and filtering unwanted files.
+This tool is essential for ensuring repository consistency and integrity across different Artifactory instances 
+especially after a SAAS migration.
+
+It has same logic as the original script in 
+https://github.com/jfrog/artifactory-scripts/blob/master/replicationDiff/replicationDiff.sh
+and uses the jfrog cli to connect to Artifactory.
+
+## Features
+
+- Fetches and compares artifacts between source and target repositories.
+- Generates reports on unique URIs present in the source but missing in the target.
+- Provides file statistics sorted by extension.
+- Supports filtering out unwanted metadata files.
+- Logs detailed information about artifact download statistics.
+
+---
+## Prerequisites
+
+Before running the script, ensure you have the following prerequisites:
+
+- Python >= 3.7
+- [Artifactory CLI (JFrog CLI)](https://www.jfrog.com/confluence/display/JFROG/JFrog+CLI) installed and configured 
+  with server IDs to access source and target JFrog Artifactory instances
+
+Note: If you are using Python 3.6 the script will fail with below stack:
+```
+   subprocess.run(command, stdout=output, stderr=subprocess.PIPE, text=True, check=True)
+ File "/usr/lib64/python3.6/subprocess.py", line 423, in run
+   with Popen(*popenargs, **kwargs) as process:
+ TypeError: __init__() got an unexpected keyword argument 'text'
+``` 
+This is due to the `text=True` argument passed to `subprocess.run` in Python. The text=True option is only available from Python 3.7 and later.
+To resolve this issue, you can modify the `subprocess.run` call to be compatible with older versions of Python by replacing `text=True` with `universal_newlines=True`. Here's the change:
+```
+# Old line causing the issue:
+subprocess.run(command, stdout=output, stderr=subprocess.PIPE, text=True, check=True)
+
+# Updated line:
+subprocess.run(command, stdout=output, stderr=subprocess.PIPE, universal_newlines=True, check=True)
+
+```
+The `universal_newlines=True` argument provides similar functionality, making the output text-based instead of bytes-based, which should resolve the issue in older versions of Python.
+
+---
+## Usage
+
+1. Download the script  [repodiff.py](repodiff.py)  to your local machine.
+
+2. Open a terminal and navigate to the directory where the script is located.
+
+3. Run the script with the following command:
+
+```bash
+   python repodiff.py --source-artifactory SOURCE_ARTIFACTORY_ID --target-artifactory TARGET_ARTIFACTORY_ID \
+      --source-repo SOURCE_REPOSITORY_NAME --target-repo TARGET_REPOSITORY_NAME \
+      [--path-in-repo  <path_within_repository>]
+```
+### Arguments:
+
+- `--source-artifactory`: The URL of the source Artifactory.
+- `--target-artifactory`: The URL of the target Artifactory.
+- `--source-repo`: The name of the source repository.
+- `--target-repo`: The name of the target repository.
+- `--path-in-repo` (optional): Path within the repository ( example: for repo docker-dev-local it can be app1/tag1 i.e the path with no repo name and ending "/" )
+
+The repodiff.py will perform the following actions:
+
+- Fetch artifact information from the source and target repositories.
+- Optionally, it can fetch data from a specific path within the repository if `--path-in-repo` parameter is provided.
+- Identify artifacts present in the source repository but missing in the target repository.
+- Calculate the total size of missing artifacts.
+- Write various output files with the results, including lists of unique URIs and statistics in the 
+  `output/<source-repo>`    directory.
+
+## Output
+
+The script generates the following output files:
+
+- **cleanpaths.txt**: Contains the URIs of artifacts present in the source repository but missing in the target repository. It also provides statistics on the total size and file extensions.
+
+- **filepaths_uri.txt**: Contains the URIs with the source repository prefix for missing artifacts.
+
+- **filepaths_nometadatafiles.txt**: The **actionable transfer list** — a filtered subset of `cleanpaths.txt` with
+  repository-managed metadata files removed (e.g. `maven-metadata.xml`, `repomd.xml`, `Packages.gz`, etc.).
+  These are the actual binaries that should be transferred from the source to the target repository.
+
+- **filepaths_uri_lastDownloaded_desc.txt**: Contains a list of unique URIs for missing artifacts. These URIs are accompanied by their download statistics ( from "artifactory.stats" ) , 
+  sorted in descending order of lastDownloaded timestamp in UTC. If an artifact has never been downloaded,  a default timestamp of **"Jan 1, 1900, 00:00:00 UTC"** is used.
+- **all_delta_paths_with_differnt_sizes.txt**: List of paths with size , sha1 mismatches or missing in the target.
+
+**Note:** `cleanpaths.txt` contains the full delta (binaries + metadata files). `filepaths_nometadatafiles.txt` is the subset with metadata files removed — these are the actual binaries to transfer.
+The excluded metadata files (those in `cleanpaths.txt` but not in `filepaths_nometadatafiles.txt`) should **not** be manually transferred because Artifactory regenerates them automatically when you
+[Recalculate the Repository Index](https://jfrog.com/help/r/artifactory-what-does-artifactory-s-recalculate-index-option-do-and-how-do-i-keep-track-of-it/recalculating-a-repository-index). Manually transferring those metadata files is unnecessary and can cause inconsistencies.
+
+You can fix the repo diff using `filepaths_nometadatafiles.txt` (recommended) with [../fix_the_repoDiff/transfer_cleanpaths_delta_from_repoDiff.py](../fix_the_repoDiff/transfer_cleanpaths_delta_from_repoDiff.py) as mentioned in [../fix_the_repoDiff/readme.md](../fix_the_repoDiff/readme.md). The script also accepts `cleanpaths.txt` but will transfer metadata files too. 
+
+Another option is to use the `compare` plugin as mentiond in https://github.com/ps-jfrog/charts/blob/master/ps/Testing_files_transfer/compare_source_to_target_and_fixrepodiff/README.md#recommended-workflow-two-pass-with-verification
+
+For Example:
+- [Calculate Maven Metadata](https://jfrog.com/help/r/jfrog-rest-apis/calculate-maven-metadata) .
+- [Calculate Alpine Repository Metadata](https://jfrog.com/help/r/jfrog-rest-apis/calculate-alpine-repository-metadata) etc
+
+## 🔁 Running `repodiff.py` for All Local Repositories
+
+The [run_repodiff_for_all_local_repos.sh](run_repodiff_for_all_local_repos.sh) script automates the process of running [repodiff.py](repodiff.py) for all local repositories in a JFrog Artifactory instance as explained in 
+[run_repodiff_for_all_local_repos.md](run_repodiff_for_all_local_repos.md)
+
+## Alternative scripts/plugin:
+- https://github.jfrog.info/projects/PROFS/repos/ps_jfrog_scripts/browse/compare_repos
+- https://github.jfrog.info/projects/PROFS/repos/jfrog-cli-plugin-compare/browse
+
+Next fix the repo diff using scritps in [../fix_the_repoDiff](../fix_the_repoDiff)
+
+---
+Note:
+
+The output of
+```text
+jf rt curl -XGET "/api/storage/APM123-att-repository-gold-local/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1"
+```
+already contains the last download statistics under "artifactory.stats." Therefore, we can utilize the `source_data` to retrieve the `mdTimestamps.artifactory.stats` for each artifact. In cases where an artifact has never been downloaded, we have the option to fall back on the "lastModified" date. However, I've chosen to assign the default last download timestamp as `"1900-01-01T00:00:00.000Z"` for artifacts that were never downloaded.
+```text
+
+{
+  "uri" : "https://proservices.jfrog.io/artifactory/api/storage/APM123-att-repository-gold-local",
+  "created" : "2023-11-08T06:02:37.392Z",
+  "files" : [ {
+    "uri" : "/",
+    "size" : -1,
+    "lastModified" : "2023-11-01T15:33:50.186Z",
+    "folder" : true
+  }, {
+    "uri" : "/org/jfrog/test/multi/4.0/multi-4.0.pom",
+    "size" : 3270,
+    "lastModified" : "2023-11-01T15:41:53.497Z",
+    "folder" : false,
+    "sha1" : "95a4881c266fd1d4679e1008754f45b19cb4da82",
+    "sha2" : "6c258cb4cf2a34eed220d3144e4c873eaefd5346f5382f07b7fd5e930bc4d97c",
+    "mdTimestamps" : {
+      "properties" : "2023-11-01T15:59:51.694Z"
+    }
+  }, {
+    "uri" : "/org/jfrog/test/multi/maven-metadata.xml",
+    "size" : 366,
+    "lastModified" : "2023-11-01T15:59:51.116Z",
+    "folder" : false,
+    "sha1" : "8aaf767b2ed90f5614ab7c600dc0dda967f43923",
+    "sha2" : "93cd16c957e5cfc44dfaebe7ac54c353ac92eea796f66b45e4bd51d0262582e9",
+    "mdTimestamps" : {
+      "artifactory.stats" : "2023-11-08T04:08:23.894Z"
+    }
+}, {
+    "uri" : "/org/jfrog/test/multi3/maven-metadata.xml",
+    "size" : 367,
+    "lastModified" : "2023-11-01T15:59:51.124Z",
+    "folder" : false,
+    "sha1" : "ffea43340e639fa5c76fe664c2a5ce87ca81f090",
+    "sha2" : "c17e801615a49a46db3d6ece16b66060d7e1084bc09ff535764cc470748e969b"
+  } ]
+}
+```
+---
+<!-->
+### Behind the scenes:
+
+I initially experimented with below scripts  which were improvised from 
+[replicationDiff.sh](https://github.com/jfrog/artifactory-scripts/blob/master/replicationDiff/replicationDiff.sh) 
+
+On mac:
+```
+bash ./replicationDiff_jf_modular_w_comm_v3.sh ncr ncratleostest fsg-th-docker-snapshots fsg-th-docker-snapshots
+```
+
+On linux:
+```
+bash ./replicationDiff_jf_modular_w_diff_v2.sh ncr ncratleostest fsg-th-docker-snapshots fsg-th-docker-snapshots
+bash ./replicationDiff_jf_modular_w_jq_v4.sh ncr ncratleostest fsg-th-docker-snapshots fsg-th-docker-snapshots
+bash ./replicationDiff_with_jf.sh ncr ncratleostest fsg-th-docker-snapshots fsg-th-docker-snapshots
+```
+
+
+Finally came up with the above  convenient python script [repodiff.py](repodiff.py):
+-->
+
+---
+
+Print all lines from the file that do not match the pattern "-202":
+```
+awk '!/-202/' filepaths_nometadatafiles.txt
+```
+
+Optional Helper commands ( not required , just FYI):
+```
+Both the following grep commands were slow on my mac to check for files not containing "-202" in the file name in the 4 MB filepaths_nometadatafiles.txt:
+
+uses regex:
+grep -v "-202" filepaths_nometadatafiles.txt
+and
+The -F flag for grep can be used to treat the search pattern as a fixed string (rather than a regular expression)
+grep -vF "-202" filepaths_nometadatafiles.txt
+
+Instead this is fast , tells awk to print all lines from the file that do not match the pattern "-202".:
+awk '!/-202/' filepaths_nometadatafiles.txt 
+
+```
+---
+#### Performance improvement in Script Functions:
+
+- **`extract_file_info(files)`**:
+  - Converts a list of files into a dictionary (hashmap) where the `uri` is the key, and the value is a tuple of the file’s `size` and `sha1` checksum.
+  - This dictionary enables constant-time complexity for lookups.
+
+- **`compare_logs(source_files, target_files)`**:
+  - Compares the source and target dictionaries by checking if each file exists in both locations and if their 
+    checksums match. Using  dictionaries to perform quick lookups and comparisons reduces the time
+    complexity to O(1) for each lookup , making it scalable for large datasets.
+  - Returns a list of `uri` paths with mismatches or missing files.
+
+---
---
+++ ps/Testing_files_transfer/repoDiff/repodiff.py
@@ -0,0 +1,296 @@
+import argparse
+import os
+import subprocess
+import json
+from datetime import datetime
+
+
+# Fetch artifacts list  from the  repository in the given artifactory.
+def fetch_repository_data(artifactory, repo, output_file, path_in_repo=None):
+    # Got the storage API params from RTDEV-34024
+    if path_in_repo:
+        # API request with path_in_repo parameter
+        command = [
+            "jf", "rt", "curl",
+            "-X", "GET",
+            f"/api/storage/{repo}/{path_in_repo}?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1",
+            "-L", "--server-id", artifactory
+        ]
+    else:
+        # API request without path_in_repo parameter
+        command = [
+            "jf", "rt", "curl",
+            "-X", "GET",
+            f"/api/storage/{repo}/?list&deep=1&listFolders=0&mdTimestamps=1&statsTimestamps=1&includeRootPath=1",
+            "-L", "--server-id", artifactory
+        ]
+    print("Executing command:", " ".join(command))
+    try:
+        with open(output_file, "w") as output:
+            subprocess.run(command, stdout=output, stderr=subprocess.PIPE, text=True, check=True)
+        print("Command executed successfully.")
+    except subprocess.CalledProcessError as e:
+        print("Command failed with error:", e.stderr)
+
+# Load the contents of the JSON files
+def load_json_file(file_path):
+    with open(file_path, 'r') as json_file:
+        return json.load(json_file)
+
+# Write the unique URIs to a file in the output folder
+def write_unique_uris(output_file, unique_uris,total_size):
+    file_extension_counts = {}
+    with open(output_file, 'w') as uri_file:
+        uri_file.write("******************************\n")
+        uri_file.write("Files present in the source repository and are missing in the target repository:\n")
+        uri_file.write("******************************\n")
+        # sorted_uris = sorted(unique_uris)
+        for uri in unique_uris:
+            uri_file.write(uri + '\n')
+            # Generate the count of files sorted by extension
+            file_extension = os.path.splitext(uri)[1]
+            file_extension_counts[file_extension] = file_extension_counts.get(file_extension, 0) + 1
+
+        # Generate and print the count of files sorted by extension to console
+        print("******************************\n")
+        print("        FILE STATS\n")
+        print("******************************\n\n")
+        print("Here is the count of files sorted according to the file extension that are present in the source repository and are missing in the target repository:")
+        for extension, count in sorted(file_extension_counts.items()):
+            print(f"{extension}: {count}")
+
+        print("Total Unique URIs in source:", len(unique_uris))
+        print("Total Size:", total_size)
+
+        # Generate and print the count of files sorted by extension to the output_file
+        uri_file.write("******************************\n")
+        uri_file.write("        FILE STATS\n")
+        uri_file.write("******************************\n\n")
+        uri_file.write("Here is the count of files sorted according to the file extension that are present in the source repository and are missing in the target repository:\n")
+        uri_file.write(f"Total Unique URIs in source: {len(unique_uris)}\n")
+        uri_file.write(f"Total Size: {total_size}\n")
+
+        for extension, count in sorted(file_extension_counts.items()):
+            uri_file.write(f"{extension}: {count}\n")
+
+
+# Write the unique URIs "with repo prefix" to a file in the output folder
+def write_unique_uris_with_repo_prefix(output_file, unique_uris, source_rt_repo_prefix):
+    with open(output_file, 'w') as uri_file:
+        for uri in unique_uris:
+            uri_file.write(source_rt_repo_prefix + "/" + uri + '\n')
+
+# Filter and write the unique URIs "without unwanted files" , to a file in the output folder
+def write_filepaths_nometadata(unique_uris,filepaths_nometadata_file):
+    with  open(filepaths_nometadata_file, "w") as filepaths_nometadata:
+        for uri in unique_uris:
+            file_name = uri.strip()
+            if any(keyword in file_name for keyword in ["maven-metadata.xml", "Packages.bz2", ".gemspec.rz",
+                                                        "Packages.gz", "Release", ".json", "Packages", "by-hash",
+                                                        "filelists.xml.gz", "other.xml.gz", "primary.xml.gz",
+                                                        "repomd.xml", "repomd.xml.asc", "repomd.xml.key"]):
+                print(f"Excluded: as keyword in {file_name}")
+            else:
+                print(f"Writing: {file_name}")
+                filepaths_nometadata.write(file_name + '\n')
+
+
+#  Get the download stats for every artifact uri in the unique_uris list from the source artifactory. But this takes
+#  a long time - 1+ hour for 13K artifacts. So use the write_artifact_stats_from_source_data function instead.
+def write_artifact_stats_sort_desc(artifactory, repo, unique_uris, output_file):
+    artifact_info = []
+    total_commands = len(unique_uris)
+
+    for i, uri in enumerate(unique_uris, start=1):
+        full_uri = f"/api/storage/{repo}/{uri.lstrip('/')}?stats"
+        command = [
+            "jf", "rt", "curl",
+            "-X", "GET",
+            full_uri,
+            "-L", "--server-id", artifactory
+        ]
+
+        print(f"Executing command {i}/{total_commands}: {' '.join(command)}")
+
+        try:
+            result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
+            print("Command executed successfully.")
+
+            # Parse the JSON response
+            response_data = json.loads(result.stdout)
+
+            # Extract relevant information
+            last_downloaded = response_data["lastDownloaded"]
+            timestamp_utc = datetime.utcfromtimestamp(last_downloaded / 1000.0).strftime('%Y-%m-%d %H:%M:%S UTC')
+
+            # Append to the list
+            artifact_info.append((uri, last_downloaded, timestamp_utc))
+        except subprocess.CalledProcessError as e:
+            print("Command failed with error:", e.stderr)
+
+    # Sort the artifact_info list in descending order of lastDownloaded
+    sorted_artifact_info = sorted(artifact_info, key=lambda x: x[1], reverse=True)
+
+    # Write the headers to the output file
+    with open(output_file, 'w') as out_file:
+        out_file.write("lastDownloaded\tTimestamp (Epoch Millis)\tURI\n")
+
+        # Write the values for each artifact in a single line
+        for uri, last_downloaded, timestamp_utc in sorted_artifact_info:
+            out_file.write(f"{last_downloaded}\t{timestamp_utc}\t{uri}\n")
+
+#  Get the download stats for every artifact uri in the unique_uris list from the mdTimestamps.artifactory.stats in the
+#  source_data json itself.
+# If the artifact was never downloaded use a default timestamp of "Jan 1 , 1900"  UTC .
+
+def write_artifact_stats_from_source_data(source_data, unique_uris, output_file):
+    artifact_info = []
+
+    # Convert source_data['files'] to a dictionary for quick lookup
+    source_files_dict = {item['uri'][1:]: item for item in source_data['files']}
+
+    for uri in unique_uris:
+        # Find the corresponding entry in source_files_dict by matching the "uri"
+        matching_entry = source_files_dict.get(uri, None)
+
+        if matching_entry:
+            # Extract the "artifactory.stats" timestamp if available, otherwise use a default timestamp of "Jan 1 , 1900"
+            timestamp_utc = matching_entry.get("mdTimestamps", {}).get("artifactory.stats", "1900-01-01T00:00:00.000Z") or "1900-01-01T00:00:00.000Z"
+
+            # Append to the list
+            artifact_info.append((uri, timestamp_utc))
+        else:
+            # If no matching entry is found, use a default timestamp
+            artifact_info.append((uri, "1900-01-01T00:00:00.000Z"))
+
+    # Sort the artifact_info list in descending order of timestamp_utc
+    sorted_artifact_info = sorted(artifact_info, key=lambda x: x[1], reverse=True)
+
+    # Write the headers to the output file
+    with open(output_file, 'w') as out_file:
+        out_file.write("Download Timestamp\tURI\n")
+
+        # Write the values for each artifact in a single line
+        for uri, timestamp_utc in sorted_artifact_info:
+            out_file.write(f"{timestamp_utc}\t{uri}\n")
+
+def extract_file_info(files):
+    return {file['uri'][1:]: (file['size'], file['sha1']) for file in files if 'sha1' in file}
+
+def compare_logs(source_files, target_files):
+    delta_paths = []
+
+    for uri, (size, sha1) in source_files.items():
+        if uri not in target_files:
+            delta_paths.append((uri, f"source=({size} , {sha1}) Not in target"))
+        elif sha1 != target_files[uri][1]:
+            delta_paths.append((uri,  f"SHA1 mismatch: source=({size} , {sha1}), target={target_files[uri]}"))
+
+    return delta_paths
+
+
+
+def write_all_filepaths_delta(delta_paths, log_path):
+    with open(log_path, 'w') as log_file:
+        for uri,  reason in delta_paths:
+            log_file.write(f"{uri}  ({reason})\n")
+
+def main():
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description="Check if repo in target Artifactory has all the artifacts from "
+                                                 "repo in source Artifactory.")
+    parser.add_argument("--source-artifactory", required=True, help="Source Artifactory ID")
+    parser.add_argument("--target-artifactory", required=True, help="Target Artifactory ID")
+    parser.add_argument("--source-repo", required=True, help="Source repository name")
+    parser.add_argument("--target-repo", required=True, help="Target repository name")
+    parser.add_argument("--path-in-repo", help="Optional parameter: Path within the repository")
+    args = parser.parse_args()
+
+    # Create the output directory if it doesn't exist
+    output_dir = f"output/{args.source_repo}"
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Fetch data from repositories
+    source_log_file = os.path.join(output_dir, "source.log")
+    fetch_repository_data(args.source_artifactory, args.source_repo, source_log_file, args.path_in_repo)
+
+    target_log_file = os.path.join(output_dir, "target.log")
+    fetch_repository_data(args.target_artifactory, args.target_repo, target_log_file, args.path_in_repo)
+
+    # Load the contents of the JSON files
+    source_data = load_json_file(source_log_file)
+    target_data = load_json_file(target_log_file)
+
+    try:
+        # Create the initial dictionary with the desired URIs and their sizes.
+        # Next, filter out URIs that start with ".jfrog" , ".npm" etc.
+        source_uris = {
+            item['uri'][1:]: [item['size'], item['sha1']]
+            for item in source_data['files']
+            if item['size'] > 0 and "_uploads/" not in item['uri'] and
+               "repository.catalog" not in item['uri'] and
+               not item['uri'][1:].startswith(".")
+        }
+    except KeyError:
+        print("Key 'files' not found in source_data. Please check the structure of the JSON file.")
+        return
+
+    try:
+        # Create the initial dictionary with the desired URIs and their sizes.
+        # Next, filter out URIs that start with ".jfrog" , ".npm" etc.
+        target_uris = {
+            item['uri'][1:]: [item['size'], item['sha1']]
+            for item in target_data['files']
+            if item['size'] > 0 and "_uploads/" not in item['uri'] and
+               "repository.catalog" not in item['uri'] and
+               not item['uri'][1:].startswith(".")
+        }
+    except KeyError:
+        print("Key 'files' not found in target_data. Please check the structure of the JSON file.")
+        target_uris = {}
+
+    # Handle the scenario when target_uris is empty or not initialized because the "--path-in-repo" does not exist in
+    # target Artifactory.
+    if not target_uris:
+        unique_uris = sorted(source_uris.keys())
+    else:
+        # Find the unique URIs that are either not in target_uris or have different 'sha1'.
+        unique_uris = sorted(
+            uri for uri, size in source_uris.items()
+            if uri not in target_uris or source_uris[uri][1] != target_uris[uri][1]
+        )
+
+    # Calculate the total size of the unique URIs.
+    total_size = sum(
+        source_uris[uri][0] for uri in unique_uris
+    )
+
+    # Write the unique URIs to a file in the output folder
+    unique_uris_file = os.path.join(output_dir, "cleanpaths.txt")
+    write_unique_uris(unique_uris_file, unique_uris, total_size)
+
+    # Write the unique URIs "with repo prefix" to a file in the output folder
+    prefix = f"{args.source_artifactory}/artifactory/{args.source_repo}"
+    filepaths_uri_file = os.path.join(output_dir, "filepaths_uri.txt")
+    write_unique_uris_with_repo_prefix(filepaths_uri_file, unique_uris, prefix)
+
+    # fetch artifact statistics, extract the relevant information, and sort the lines in descending order of the lastDownloaded timestamp
+    # to a file in the output folder
+    filepaths_uri_stats_file=os.path.join(output_dir, "filepaths_uri_lastDownloaded_desc.txt")
+    # write_artifact_stats_sort_desc(args.source_artifactory, args.source_repo, unique_uris, filepaths_uri_stats_file)
+    write_artifact_stats_from_source_data( source_data, unique_uris,
+                                           filepaths_uri_stats_file)
+
+    # Filter and write the unique URIs "without unwanted files" , to a file in the output folder
+    filepaths_nometadata_file = os.path.join(output_dir, "filepaths_nometadatafiles.txt")
+    write_filepaths_nometadata(unique_uris, filepaths_nometadata_file)
+
+    source_files = extract_file_info(source_data['files'])
+    target_files = extract_file_info(target_data['files'])
+
+    delta_paths = compare_logs(source_files, target_files)
+    delta_log_path = os.path.join(output_dir, "all_delta_paths_with_differnt_sizes.txt")
+    write_all_filepaths_delta(delta_paths, delta_log_path)
+
+if __name__ == "__main__":
+    main()
